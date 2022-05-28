ba1623f7caaf486c4e2bad43cd0189549d7c0c5b
asialiugf <2337957910@qq.com>
2022-05-28 10:00:52 +0800

    init project

---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import unocss from 'unocss/vite';
+
+export default defineConfig(configEnv => {
+  const viteEnv = loadEnv(configEnv.mode, `.env.${configEnv.mode}`);
+
+  const isVercel = viteEnv.VITE_IS_VERCEL === '1';
+
+  return {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    plugins: [
+      vue(),
+      unocss({ include: ['src/App.vue'] }),
+      dts({
+        include: ['./src/index.ts', './src/index.vue'],
+        beforeWriteFile(filePath, content) {
+          return {
+            filePath: filePath.replace('/dist/src/', '/dist/'),
+            content
+          };
+        }
+      })
+    ],
+    optimizeDeps: {
+      exclude: ['vue-demi']
+    },
+    build: isVercel
+      ? {
+          brotliSize: false
+        }
+      : {
+          lib: {
+            entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+            name: 'SoybeanAdminLayout',
+            fileName: 'index'
+          },
+          rollupOptions: {
+            external: ['vue'],
+            output: {
+              globals: {
+                vue: 'Vue'
+              }
+            }
+          }
+        }
+  };
+});
