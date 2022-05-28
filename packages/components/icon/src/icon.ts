d7e715bd4ef721be00dea52c58a44ac2313f85c0
asialiugf <2337957910@qq.com>
2022-05-28 14:34:10 +0800

    feat(components): 增加XIcon组件

---
+++ packages/components/icon/src/icon.ts
@@ -0,0 +1,10 @@
+import type { ExtractPropTypes } from "vue";
+export const iconProps = {
+  size: {
+    type: Number,
+  },
+  color: {
+    type: String,
+  },
+} as const;
+export type IconProps = ExtractPropTypes<typeof iconProps>;
--- packages/test01/src/App.vue
+++ packages/test01/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
@@ -8,6 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <x-icon color="red" :size="30">你好</x-icon>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
