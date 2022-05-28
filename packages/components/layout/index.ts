b5ae957faccf7295ab52bb824f779af40fd4d633
asialiugf <2337957910@qq.com>
2022-05-28 16:10:55 +0800

    feat(components): 增加Layout组件

---
+++ packages/components/layout/index.ts
@@ -0,0 +1,46 @@
+import Layout from "./src/Layout.vue";
+import { withInstall } from "@asialine/utils/with-install";
+
+const XLayout = withInstall(Layout);
+export { XLayout };
+export default XLayout;
+// 两种导出方式
+//-----------------------------------------------------------------------
+
+export interface LayoutProps {
+  /** 布局模式 */
+  mode?: 'vertical' | 'horizontal';
+  /** 最小宽度 */
+  minWidth?: number;
+  /** 头部可见 */
+  headerVisible?: boolean;
+  /** 头部高度 */
+  headerHeight?: number;
+  /** 标签可见 */
+  tabVisible?: boolean;
+  /** 标签页高度 */
+  tabHeight?: number;
+  /** 固定头部和标签 */
+  fixedHeaderAndTab?: boolean;
+  /** 给主体添加禁止溢出 */
+  addMainOverflowHidden?: boolean;
+  /** 底部可见 */
+  footerVisible?: boolean;
+  /** 底部高度 */
+  footerHeight?: number;
+  /** 固定底部 */
+  fixedFooter?: boolean;
+  /** 侧边可见 */
+  siderVisible?: boolean;
+  /** 侧边栏高度 */
+  siderWidth?: number;
+  /** 侧边栏折叠状态的高度 */
+  siderCollapsedWidth?: number;
+  /** 侧边栏折叠状态 */
+  siderCollapse?: boolean;
+  /** 动画过渡时间 */
+  transitionDuration?: number;
+  /** 动画过渡速度曲线 */
+  transitionTimingFunction?: string;
+}
+
--- xia/src/App.vue
+++ xia/src/App.vue
@@ -1,119 +1,75 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from '@/components/HelloWorld.vue';
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <admin-layout
+    :mode="mode"
+    :fixed-header-and-tab="fixedHeaderAndTab"
+    :fixed-footer="fixedFooter"
+    :sider-collapse="siderCollapse"
+  >
+    <template #header>
+      <div class="flex-center h-full bg-#e6e6e6">Header</div>
+    </template>
+    <template #tab>
+      <div class="flex-center h-full bg-#cccccc">Tab</div>
+    </template>
+    <template #sider>
+      <div class="h-full px-12px bg-#d9d9d9 whitespace-nowrap">
+        <div class="flex-center h-56px">Sider</div>
+        <div>
+          <h4>layout mode:</h4>
+          <div v-for="item in modeList" :key="item">
+            <span class="pr-8px">{{ item }}</span>
+            <input
+              type="radio"
+              name="mode"
+              :value="item"
+              :checked="item === mode"
+              class="cursor-pointer"
+              @change="setMode(item)"
+            />
+          </div>
+        </div>
+        <div class="pt-24px">
+          <span class="pr-8px">fixedHeaderAndTab</span>
+          <input type="checkbox" :checked="fixedHeaderAndTab" @change="setFixedHeaderAndTab" />
+        </div>
+        <div class="pt-24px">
+          <span class="pr-8px">fixedFooter</span>
+          <input type="checkbox" :checked="fixedFooter" @change="setFixedFooter" />
+        </div>
+        <div class="pt-24px">
+          <span class="pr-8px">siderCollapse</span>
+          <input type="checkbox" :checked="siderCollapse" @change="setSiderCollapse" />
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <div class="flex-center h-full bg-#e6e6e6">Footer</div>
+    </template>
+    <div v-for="i in 100" :key="i" class="text-center">{{ i }}</div>
+  </admin-layout>
 </template>
 
-<style>
-@import '@/assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+<script setup lang="ts">
+import { ref } from 'vue';
+import AdminLayout from '@asialine/xia-ui/layout';
+type Mode = 'vertical' | 'horizontal';
+const mode = ref<Mode>('vertical');
+const modeList: Mode[] = ['vertical', 'horizontal'];
+function setMode(value: Mode) {
+  mode.value = value;
 }
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+const fixedHeaderAndTab = ref(true);
+function setFixedHeaderAndTab() {
+  fixedHeaderAndTab.value = !fixedHeaderAndTab.value;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+const fixedFooter = ref(false);
+function setFixedFooter() {
+  fixedFooter.value = !fixedFooter.value;
 }
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
+const siderCollapse = ref(false);
+function setSiderCollapse() {
+  siderCollapse.value = !siderCollapse.value;
 }
+</script>
 
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style></style>
