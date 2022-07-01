e6f0af9e25972c7e9bc15d7a49475b6bef96fd85
asialiugf <2337957910@qq.com>
2022-07-01 21:52:01 +0800

    refactor(components): asideItem interface调整

--- packages/components/layout/index.ts
+++ packages/components/layout/index.ts
@@ -1,12 +1,14 @@
 import { withInstall } from '@asialine/utils/with-install';
 import Layout from './src/Layout.vue';
+import type { asideItem } from './src/composables/useAsideList';
 // import type { asideType } from './src/XLayout.vue';
 // import asideType from './src/XLayout.vue';
 
 const YLayout = withInstall(Layout);
 export { YLayout };
 export default YLayout;
-// export type { asideType };
+
+export type { asideItem };
 
 // 两种导出方式
 //-----------------------------------------------------------------------
@@ -32,27 +34,27 @@ export interface LayoutProps {
   transitionTimingFunction?: string;
 }
 
-export interface asideItem {
-  slotPosition: 'absolute' | 'sticky'; // LayoutAside使用 ： 从应用程序传过来，并将其传至 LayoutAside子组件中。
-  key: string; // 侧边栏的key，slot的name会用这个key  要唯一，不能重复
-  header: 'cover' | 'hidden' | 'header' | 'tab' | 'none'; // 是否覆盖 header ?
-  footer: boolean; // 是否覆盖 footer
-  side: 'left' | 'right'; // 停靠方式： 'left' 左对齐 'right' 右对齐 'mainl' 主区 左对齐 'mainr' 主区 右对齐 'isolated' 单独定位
-  display?: number; // 2:显示， 1：不显示：屏幕宽度变窄，0：不显示：人为设定不显示。默认为2。
-  draggbale?: boolean; // 是否可以移动
-  width: number;
-  height?: number;
-  start?: number;
-  end?: number;
-  top?: number | 'auto';
-  left?: number | 'auto';
-  right?: number | 'auto';
-  bottom?: number | 'auto';
-  zIndex?: number;
-  coverType?: number; // 覆盖类型，0:-cover，1:-hidden，2:-header，3:-tab，4:-none 用于排序
-  slotTop?: number;
-  slotHeight?: number;
-}
+// export interface asideItem {
+//   slotPosition: 'absolute' | 'sticky'; // LayoutAside使用 ： 从应用程序传过来，并将其传至 LayoutAside子组件中。
+//   key: string; // 侧边栏的key，slot的name会用这个key  要唯一，不能重复
+//   header: 'cover' | 'hidden' | 'header' | 'tab' | 'none'; // 是否覆盖 header ?
+//   footer: boolean; // 是否覆盖 footer
+//   side: 'left' | 'right'; // 停靠方式： 'left' 左对齐 'right' 右对齐 'mainl' 主区 左对齐 'mainr' 主区 右对齐 'isolated' 单独定位
+//   display?: number; // 2:显示， 1：不显示：屏幕宽度变窄，0：不显示：人为设定不显示。默认为2。
+//   draggbale?: boolean; // 是否可以移动
+//   width: number;
+//   height?: number;
+//   start?: number;
+//   end?: number;
+//   top?: number | 'auto';
+//   left?: number | 'auto';
+//   right?: number | 'auto';
+//   bottom?: number | 'auto';
+//   zIndex?: number;
+//   coverType?: number; // 覆盖类型，0:-cover，1:-hidden，2:-header，3:-tab，4:-none 用于排序
+//   slotTop?: number;
+//   slotHeight?: number;
+// }
 
 export interface barsType {
   cover: { left: number; width: number };
--- packages/components/layout/src/composables/useAsideList/index.ts
+++ packages/components/layout/src/composables/useAsideList/index.ts
@@ -3,7 +3,29 @@ import { ref } from 'vue';
 import type { Ref } from 'vue';
 import { tryOnMounted, useEventListener, defaultWindow } from '@vueuse/core';
 import type { ConfigurableWindow } from '@vueuse/core';
-import type { asideItem, barsType } from '@asialine/xia-ui/layout';
+import type { barsType } from '@asialine/xia-ui/layout';
+
+export interface asideItem {
+  slotPosition: 'absolute' | 'sticky'; // LayoutAside使用 ： 从应用程序传过来，并将其传至 LayoutAside子组件中。
+  key: string; // 侧边栏的key，slot的name会用这个key  要唯一，不能重复
+  header: 'cover' | 'hidden' | 'header' | 'tab' | 'none'; // 是否覆盖 header ?
+  footer: boolean; // 是否覆盖 footer
+  side: 'left' | 'right'; // 停靠方式： 'left' 左对齐 'right' 右对齐 'mainl' 主区 左对齐 'mainr' 主区 右对齐 'isolated' 单独定位
+  display?: number; // 2:显示， 1：不显示：屏幕宽度变窄，0：不显示：人为设定不显示。默认为2。
+  draggbale?: boolean; // 是否可以移动
+  width: number;
+  height?: number;
+  start?: number;
+  end?: number;
+  top?: number | 'auto';
+  left?: number | 'auto';
+  right?: number | 'auto';
+  bottom?: number | 'auto';
+  zIndex?: number;
+  coverType?: number; // 覆盖类型，0:-cover，1:-hidden，2:-header，3:-tab，4:-none 用于排序
+  slotTop?: number;
+  slotHeight?: number;
+}
 
 // 侧边栏列表， 接口的内容一部分是从应用程序中传过来。一部分是计算出来。
 // 侧边栏显示，分为两部分，一部分在本VUE中，position为absolute,参见 【:style="asideStyle(item)"】
